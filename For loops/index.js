ae4b8e0accdf8986bfebbcd11c1de5a90ab23518
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-12-09 16:15:10 +0600

    Add files via upload

---
+++ For loops/index.js
@@ -0,0 +1,53 @@
+//Loops
+
+// for(let i = 0; i < 10; i++){
+//     console.log((i + 1) + ` Biddut`)
+
+// }
+
+//for(initializer, condition, increment) {
+//
+//}
+
+// for(let i = 1; i <= 100; i++) {
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 32; i++) {
+//     console.log(sum + ' + ' + i + ' = ' + (sum+i))
+//     sum +=i
+// }
+
+// console.log('Result = ' + sum)
+
+
+// for(let i = 10; i >= 0; 2 - i){
+//     console.log(i)
+// }
+
+
+
+
+//-----------------------------while loops 
+// let text = "";
+// let i = 0;
+// while(i < 100){
+//     text += i; 
+//     i++;
+// }
+// console.log(text)
+
+
+let text = '';
+let i = 0;
+
+do {
+    text += i; 
+    i++;
+}
+while(i< 30);
+
+console.log(text)
